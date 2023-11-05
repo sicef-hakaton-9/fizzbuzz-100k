429b0cfec77560b08ecc13623b850e36545523ef
Dusan Velickovic <dusanvelickovic003@gmail.com>
2023-11-05 11:02:07 +0100

    client redirect fix

--- client/src/components/Homepage/Header/Header.tsx
+++ client/src/components/Homepage/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const navigate = useNavigate();
 
   const handleGoRegister = () => {
-    navigate("/create-acc");
+    navigate("/sign-in");
   };
   return (
     <header className={styles.header}>
